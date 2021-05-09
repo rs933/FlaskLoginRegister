a849a10944706e4ccc296fd3a590a817c322d560
rs933 <rs933@njit.edu>
2021-05-09 01:57:41 -0400

    added new changes

--- app/app.py
+++ app/app.py
@@ -77,6 +77,10 @@ def form_delete_post(city_id):
     mysql.get_db().commit()
     return redirect("/", code=302)
 
+@app.route('/login/register/', methods=['POST', 'GET'])
+def login_register():
+    return render_template('login_register.html')
+
 
 @app.route('/api/v1/cities', methods=['GET'])
 def api_browse() -> str:
